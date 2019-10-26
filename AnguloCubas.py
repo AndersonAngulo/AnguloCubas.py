c93c7743086d46e74f91aa7fd5f7f39702f70505
AndersonAngulo <56948485+AndersonAngulo@users.noreply.github.com>
2019-10-25 22:55:54 -0500

    Create AnguloCubas.py

---
+++ AnguloCubas.py
@@ -0,0 +1,530 @@
+primer_nombre="Piero"
+segundo_nombre="Anderson"
+sexo="masculino"
+print(primer_nombre)
+print(segundo_nombre)
+print(sexo)
+print("Yo soy "+ primer_nombre , segundo_nombre , ", de sexo " , sexo)
+
+primer_apellido="Angulo"
+segundo_apellido="Cubas"
+edad=17
+print(primer_apellido)
+print(segundo_apellido)
+print(str(edad))
+print("Mi apellido completo es ", primer_apellido  , segundo_apellido , ",y tengo " , str(edad))
+
+
+distrito="Chiclayo"
+provincia="Chiclayo"
+departamento="Lambayeque"
+print(distrito)
+print(provincia)
+print(departamento)
+print("Naci en  ", distrito ,"que pertenece a la provincia de" ,provincia , ",y al departamento de " , departamento)
+
+DNI=75757856
+estado_civil="soltero"
+direccion="Los girasoles 217"
+print(str(DNI))
+print(estado_civil)
+print(direccion)
+print("Mi numero de DNI es ", str(DNI) ,", soy " , estado_civil ,",y resido en la calle" , direccion)
+
+caducidad="17/05/2021"
+emision="17/05/2016"
+nacimiento="17/04/2002"
+print(caducidad)
+print(emision)
+print(nacimiento)
+print("La fecha de emision de mi DNI es ", emision, "y caduca el " , caducidad ,",ademas nací en la fecha " , nacimiento)
+
+color="negro"
+marca="Dodge"
+modelo="Challenger"
+print(color)
+print(marca)
+print(modelo)
+print("mi auto es un ", marca , modelo, "de color " , color)
+
+dueno="Mario Alvarado"
+torque="360 Nm"
+potencia="330 hp"
+print(dueno)
+print(torque)
+print(potencia)
+print("El sr. ", dueno ,"tiene un auto cuyo torque y potencia es ",torque,",",potencia , "respectivamente" )
+
+cilindrada="1,598cc"
+traccion="4x2 delantera"
+frenos="ABS + EBD + BA"
+print(cilindrada)
+print(traccion)
+print(frenos)
+print("presenta una cilindrada de ",cilindrada, ", una traccion igual", traccion ,"y frenos " , frenos)
+
+combustible="gasolina"
+placa="101MC4"
+ano1=2006
+print(combustible)
+print(placa)
+print(ano1)
+print("el auto de placa ", placa ,",del año" ,str(ano1) , "usa como combustible: " ,combustible)
+
+numero_licencia="E10721425"
+clase="A"
+categoria="I"
+print(numero_licencia)
+print(clase)
+print(categoria)
+print(" la licencia  ", clase , categoria , ",cuyo numero es" , numero_licencia)
+
+s_operativo_cel="ANDROID"
+RAM="2 GB"
+Procesador_cel="Snapdragon"
+print(s_operativo_cel)
+print(RAM)
+print(Procesador_cel)
+print("el celular ", s_operativo_cel ,"presenta", RAM, ",y un procesador " , Procesador_cel)
+
+s_operativo_pc="WINDOWS"
+procesador_pc="Intel core i5"
+disco_duro="1 TB"
+print(s_operativo_pc)
+print(procesador_pc)
+print(disco_duro)
+print("el ordenador con sistema operativo ", s_operativo_pc ,"presenta un disco duro de", disco_duro , ",y un procesador " , procesador_pc)
+
+arquitectura="64 bits"
+tarjeta_video="NVIDIA RTX"
+version_so_pc="PRO"
+print(arquitectura)
+print(tarjeta_video)
+print(version_so_pc)
+print("presenta una arquitectura de ", arquitectura,"con trajeta de video",tarjeta_video , ", ademas de con la version de windows 10" , version_so_pc)
+
+version_so_cel=7.0
+modelo_cel="J7"
+marca_cel="Samsung"
+print(version_so_cel)
+print(modelo_cel)
+print(marca_cel)
+print("el celular ", marca_cel , modelo_cel , ", presenta su sistema operativo con la version " , str(version_so_cel))
+
+amperaje="1 A"
+voltaje_min=3.8
+voltaje_max=4.3
+print(amperaje)
+print(voltaje_min)
+print(voltaje_max)
+print("amperaje del cagador ", amperaje ,".Recibe la bateria un voltaje min para que encienda de",str( voltaje_min) , "y uno maximo de " , str(voltaje_max))
+
+porcentaje_bateria="25%"
+fecha="22/10/2019"
+hora="10:09"
+print(porcentaje_bateria)
+print(fecha)
+print(hora)
+print("porcentaje de bateria:", porcentaje_bateria ,"\n Fecha:", fecha, "\n Hora: " , hora)
+
+v_internet="3MB"
+f_inalambrica="2.4 Ghz"
+operadora="Movistar"
+print(v_internet)
+print(f_inalambrica)
+print(operadora)
+print("internet con velocidad de ", v_internet ,",wifi con una frecuencia de", f_inalambrica , ", gracias a la operadora",operadora)
+
+numero_cel=990055926
+imei=1234567897456123
+cant_datos="1 mega"
+print(numero_cel)
+print(imei)
+print(cant_datos)
+print("el ceular con imei ", str(imei),"cuyo numero ce contacto es", str(numero_cel) , ", presenta cierta cantidad de datos de " , cant_datos)
+
+colegio="CIMA"
+ano_ingreso="2013"
+ano_egreso="2018"
+print(colegio)
+print(ano_ingreso)
+print(ano_egreso)
+print("ingreso en el ano ", ano_ingreso ,"al colegio", colegio , ", y egreso en el ano " ,ano_egreso)
+
+nombre_madre="Maritza"
+apellido_madre="Cubas Sotelo"
+edad="50"
+print(nombre_madre)
+print(apellido_madre)
+print(edad)
+print("ella es ", nombre_madre, apellido_madre , ", y tiene " , str(edad) ,"años")
+
+nombre_padre="Rafael"
+apellido_padre="Angulo Carranza"
+edad1="50"
+print(nombre_padre)
+print(apellido_padre)
+print(edad1)
+print("el es ", nombre_padre,apellido_padre, ", y tiene " , str( edad1 ),"años")
+
+alumno="Anderson Angulo"
+seccion_culmino="5to 'A'"
+sexo1="masculino"
+print(alumno)
+print(seccion_culmino)
+print(sexo1)
+print("el alumno ", alumno ,"de sexo", sexo1 , ", culmino en la seccion de " , seccion_culmino)
+
+RUC=10167772459
+razon_social='"QUIROGA S.A.C"'
+numero_factura=1213132
+print(RUC)
+print(razon_social)
+print(numero_factura)
+print("en la factura de numero", str(numero_factura),"de la empresa", razon_social , "y con RUC " ,RUC)
+
+nombre_producto="carburadores"
+precio_unitario="20 soles"
+cant_produc="15 "
+print(nombre_producto)
+print(precio_unitario)
+print(cant_produc)
+print("comprare ", cant_produc , nombre_producto , "a " , precio_unitario)
+
+direccion_empresa="LOS SAUCES 1102"
+direccion_adquirente="LA PRIMAVERA MZ A LOTE 5"
+telefono_adquirente="261748"
+print(direccion_empresa)
+print(direccion_adquirente)
+print(telefono_adquirente)
+print("se registra que la direccion de la empresa es", direccion_empresa ,",ademas, del adquirente ", direccion_adquirente , "y el telefono de este ultimo " , telefono_adquirente)
+
+titulo_obra="La casa verde"
+autor="Mario Vargas Llosa"
+editorial="Alfaguara"
+print(titulo_obra)
+print(autor)
+print(editorial)
+print("La obra", titulo_obra ,"del autor", autor , ", fue impresa por al editorial " ,editorial)
+
+marca_usb="kiNGSTON"
+precio_usb="25 soles"
+almacenamiento_usb="16 GB"
+print(marca_usb)
+print(precio_usb)
+print(almacenamiento_usb)
+print("el usb", marca_usb ,"de ", almacenamiento_usb, "me costo " , precio_usb)
+
+peso="65 kg"
+talla="1.80 m"
+edad_paciente="40 años"
+print(peso)
+print(talla)
+print(edad_paciente)
+print("el paciente de", edad_paciente ,"pesa", peso , "y mide " ,talla)
+
+tipo_sagre="o negativo"
+globulos_rojos="5.900.000/mL"
+globulos_blancos="11.000/ml"
+print(tipo_sagre)
+print(globulos_rojos)
+print(globulos_blancos)
+print("presenta ", globulos_rojos,"globulos rojos. Presenta", globulos_blancos, "globulos blancos " , "y su tipo de sangre es",tipo_sagre)
+
+plaquetas="450.000 /ml"
+hemoglobina="32 pg"
+urea="1.5 mg/dl"
+print(plaquetas)
+print(hemoglobina)
+print(urea)
+print("presenta ", plaquetas,"plaquetas. Presenta", hemoglobina, "de hemoglobina " , "y de urea",urea)
+
+acido_urico="7 mg/dl"
+trigliceridos="100 mg/dl"
+glucosa="100 mg/dl."
+print(acido_urico)
+print(trigliceridos)
+print(glucosa)
+print("presenta ", acido_urico,"de acido urico.Presenta", trigliceridos, "trigliceridos" , "y de glucosa",glucosa)
+
+colesterol="150 mg/dl"
+creatinina="90 ml/min"
+calcio="10.5 mg/dl"
+print(colesterol)
+print(creatinina)
+print(calcio)
+print("presenta ", colesterol,"de coleresterol. Presenta", creatinina, "de creatinina " , "y de calcio ",calcio)
+
+hierro="50 mg/dl"
+potasio="4.5 mol/litro"
+sodio="145 mol/litro"
+print(hierro)
+print(potasio)
+print(sodio)
+print("presenta ", hierro,"de hierro.Presenta", potasio, "de potasio " , "y de sodio",sodio)
+
+universidad="Pedro Ruiz Gallo"
+facultad="FACFYM"
+escuela_profe="ing.electronica"
+print(universidad)
+print(facultad)
+print(escuela_profe)
+print("Estdudia en la universidad", universidad,",en la facultad de ", facultad, "y ademas pertence a la escuela de",escuela_profe)
+
+asignatura="Programacion"
+horario="7:30-10:00"
+aula=14
+print(asignatura)
+print(horario)
+print(aula)
+print("el curso de",asignatura,"nos tocara en el horario de", horario , "en el aula " , str(aula))
+
+tema="funciones"
+nombre_docente="Augusto Ramos"
+ciclo="I"
+print(tema)
+print(nombre_docente)
+print(ciclo)
+print("El profesor ", nombre_docente,"enseñara el tema de", tema, ",dentro del ciclo " , ciclo)
+
+dia=17
+mes="julio"
+ano=2019
+print(dia)
+print(mes)
+print(ano)
+print(str(dia) ,"de", mes, "del " , str(ano))
+
+nombre_alumno="Carlos"
+apellido_alumno="Guayan Carmen"
+codigo=12456
+print(nombre_alumno)
+print(apellido_alumno)
+print(codigo)
+print("el polo ", primer_nombre ,"de talla", segundo_nombre , ", presenta un porcentaje de algodon de " , sexo)
+
+nombre_ganador="Anderson"
+puesto="1ero"
+nombre_concurso='"Copa Creciendo con el futbol"'
+print(nombre_ganador)
+print(puesto)
+print(nombre_concurso)
+print("en el concurso ", nombre_concurso,"gano el ", puesto , ", el muchacho " , nombre_ganador)
+
+nombre_canal="derivando"
+duracion="10 min"
+titulo_video="integrales"
+print(distrito)
+print(provincia)
+print(departamento)
+print("el canal de youtube llamado",nombre_canal ,"tienes un video llamado", titulo_video, ", con una duracion de " , duracion)
+
+vistas=5200000
+suscriptores="1M"
+likes="5K"
+print(vistas)
+print(suscriptores)
+print(likes)
+print("el video del canal con ", suscriptores ,"presenta", likes , "y" , str(vistas),"vistas")
+
+fecha_nacimiento="17/04/02"
+fecha_defuncion="25/06/50"
+causa_muerte="traumatismo"
+print(fecha_nacimiento)
+print(fecha_defuncion)
+print(causa_muerte)
+print("nacio el", fecha_nacimiento ,",fallecio", fecha_defuncion , ",murio por" , causa_muerte)
+
+enviados=20
+recibidos=15
+borradores=4
+print(enviados)
+print(recibidos)
+print(borradores)
+print("enviados", str(enviados) ,",recibidos", str(recibidos) , ", borradores " , str(borradores))
+
+destacados=7
+spam=4
+pospuestos=6
+print(destacados)
+print(spam)
+print(pospuestos)
+print("destacados", str(destacados) ,",mensajes spam", str(spam) , ", pospuestos " , str(pospuestos))
+
+bandeja_salida=6
+papelera=10
+programados=3
+print(bandeja_salida)
+print(papelera)
+print(programados)
+print("mensajes en la bandeja de salida ", str(bandeja_salida) ,", en la papelera", str(papelera) , "y programados " , str(programados))
+
+marca_ropa="PUMA"
+porcentaje_algodon="60%"
+talla="S"
+print(marca_ropa)
+print(porcentaje_algodon)
+print(talla)
+print("el polo ", marca_ropa ,"de talla", talla , ", presenta un porcentaje de algodon de " , porcentaje_algodon)
+
+fecha_de_vencimiento="17/05/20"
+nombre_del_producto="papaleta"
+nacionalidad_producto="ecuatoriano"
+print(fecha_de_vencimiento)
+print(nombre_del_producto)
+print(nacionalidad_producto)
+print("la ", nombre_del_producto ,"es", nacionalidad_producto , "y se vence el " , fecha_de_vencimiento)
+
+color_ojos="azules"
+color_cabello="castaño"
+forma_rostro="cuadrado"
+print(color_ojos)
+print(color_cabello)
+print(forma_rostro)
+print("ojos ", color_ojos ,",cabello", color_cabello , "y rostro" , forma_rostro)
+
+forma_nariz="perfilada"
+tamano_labios="gruesos"
+forma_pestana="rizadas"
+print(forma_nariz)
+print(tamano_labios)
+print(forma_pestana)
+print("su nariz ", forma_nariz,"con labios" ,tamano_labios , ",y pestañas" , forma_pestana)
+
+color_piel="canela"
+tamano_dientes="medianos"
+tamano_orejas="medianas"
+print(color_piel)
+print(tamano_dientes)
+print(tamano_orejas)
+print("color de piel ", color_piel , "orejas",tamano_orejas , ", y  dientes " , tamano_dientes)
+
+minutos=50
+equipos="Uru-Per"
+marcador="0-2"
+print(minutos)
+print(equipos)
+print(marcador)
+print("los equipos", equipos, "estan en el minuto ", minutos , "con el marcador " , marcador)
+
+nombre_jugador="Guerero"
+numero_camiseta=9
+nombre_club="Inter"
+print(nombre_jugador)
+print(numero_camiseta)
+print(nombre_club)
+print("el furbolista ", nombre_jugador,"con camiseta numero", str(numero_camiseta) , "del " , nombre_club)
+
+color_camiseta="negra"
+auspiciador="Banco Pichincha"
+marca_camiseta="Umbro"
+print(color_camiseta)
+print(auspiciador)
+print(marca_camiseta)
+print("la camiseta ", color_camiseta , marca_camiseta, ",auspicidada por ", auspiciador)
+
+pelicula="Tom Holland"
+director="Jon Watts"
+estudio_cine="MARVEL STUDIOS"
+print(pelicula)
+print(director)
+print(estudio_cine)
+print("la pelicula", pelicula , "fue dirigida por" ,director, "en el estudio cinematrografico de " , estudio_cine)
+
+duracion="125 min"
+taquilla="1M"
+presupuesto=5000000
+print(duracion)
+print(taquilla)
+print(presupuesto)
+print("se gastó ", presupuesto , "para", duracion,"pero alacanzo un taquilla de",taquilla)
+
+elemento="cobre"
+numero_atomico=56
+numero_masa=56.467
+print(elemento)
+print(numero_atomico)
+print(numero_masa)
+print("el", elemento ,"pesa", str(numero_masa) , ", y su numero atomico es " , str(numero_atomico))
+
+nombre_postulante="Alonso Rivas"
+codigo_postulante=2010057
+hora_examen="8:00 am"
+print(nombre_postulante)
+print(codigo_postulante)
+print(hora_examen)
+print("el postulante", nombre_postulante ," con el codigo", codigo_postulante , ", rendira su examen a las " , hora_examen)
+
+nombre_tienda="PEPE"
+pagina_web="www.pepes.com"
+direccion_tienda="los algarrobos 205"
+print(nombre_tienda)
+print(pagina_web)
+print(direccion_tienda)
+print("a la tienda  ", nombre_tienda, "la puedes ubicar en la calle",direccion_tienda , ", o en la pagina " , pagina_web)
+
+numero_boleta=45545
+fecha_compra="17/08/19"
+hora_compra="5:35 pm"
+print(numero_boleta)
+print(fecha_compra)
+print(hora_compra)
+print("la fecha de compra es ", fecha_compra , " y la hora " ,hora_compra , ",ademas el numero de boleta es: " , str(numero_boleta))
+
+nombre_imprenta="arcolor"
+direccion_imprenta="las americas 512"
+propietario_imprenta="Mario Colores"
+print(nombre_imprenta)
+print(direccion_imprenta)
+print(propietario_imprenta)
+print("el señor", propietario_imprenta ,"es dueño de la imprenta" ,nombre_imprenta, "que se ubica en la calle" , direccion_imprenta)
+
+N_certificado=6546548944
+N_profesora="Maria Suclupe"
+duracion_capaci="3 horas"
+print(N_certificado)
+print(N_profesora)
+print(duracion_capaci)
+print("el numero de certificado de capacitacio, es", N_certificado ,".Fue dictada por la maestra" ,N_profesora , "y duro " , duracion_capaci)
+
+frecuencia_pagos="mensual"
+N_cuotas="12"
+monto_aprobado="2000 soles"
+print(frecuencia_pagos)
+print(N_cuotas)
+print(monto_aprobado)
+print("pagare un monto de", monto_aprobado , "en cuotas de",str(N_cuotas) ,"con frecuencia" , frecuencia_pagos)
+
+ocupacion_padre="policia"
+e_c_padre="casado"
+nombre_del_padre="Pablo"
+print(ocupacion_padre)
+print(e_c_padre)
+print(nombre_del_padre)
+print("mi papa se llama ", nombre_del_padre ,"y es" , e_c_padre, ",ademas es " , ocupacion_padre)
+
+
+ocupacion_madre="enfermera"
+e_c_madre="casada"
+nombre_de_la_madre="_Juana"
+print(ocupacion_madre)
+print(e_c_madre)
+print(nombre_de_la_madre)
+print("mi mama se llama ", nombre_de_la_madre ,"y es" , e_c_madre, ",ademas es " , ocupacion_madre)
+
+DNI_la_conyuge=141565656
+nombre_la_conyuge="Paola"
+edad_la_conyuge=30
+print(DNI_la_conyuge)
+print(nombre_la_conyuge)
+print(edad_la_conyuge)
+print("el señor", nombre_la_conyuge,"tiene", str(edad_la_conyuge), ", y su numero de DNI es:" , DNI_la_conyuge)
+
+
+DNI_el_conyuge=167777248
+nombre_el_conyuge="Juan"
+edad_el_conyuge=30
+print(DNI_el_conyuge)
+print(nombre_el_conyuge)
+print(edad_el_conyuge)
+print("la señora", nombre_el_conyuge,"tiene",str( edad_el_conyuge), ", y su numero de DNI es:" , DNI_el_conyuge)
